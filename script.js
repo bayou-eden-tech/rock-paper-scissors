0c14b0ac35faf3e0e55c817fc5056e47f15c68fc
Jeremiah Johnson <bayou.eden.tech@gmail.com>
2022-09-07 11:34:39 -0500

    Create if statements declaring which items win

--- script.js
+++ script.js
@@ -2,24 +2,39 @@ function getComputerChoice() {
    
     var array = ['Rock', 'Paper', 'Scissors'];
     let random = Math.floor(Math.random()*array.length);
-    let choice = "";
-
-
+    let compChoice = "";
 
     switch(random) {
         case 0:
-            choice = "Rock";
+            compChoice = "Rock";
             break;
         case 1:
-            choice = "Paper";
+            compChoice = "Paper";
             break;
         case 2:
-            choice = "Scissors"
+            compChoice = "Scissors"
     }
 
-
-console.log(choice)
+console.log(compChoice)
  
+
+    let userChoice = prompt("Enter a selection: Rock, Paper, or Scissors.");
+
+    if (userChoice == compChoice) {
+        alert("Tie")
+    } else if ((userChoice == 'rock') && (compChoice == 'Scissors')) {
+        alert("Rock beats Scissors! YOU WIN!")
+    } else if ((userChoice == 'rock') && (compChoice == 'Paper')) {
+        alert("Paper beats Rock! YOU LOSE!")
+    } else if ((userChoice == 'paper') && (compChoice == 'rock')) {
+        alert("Paper beats Rock! YOU WIN!")
+    } else if ((userChoice == 'paper') && (compChoice == 'scissors')) {
+        alert("Scissors beats Paper! YOU LOSE!")
+    } else if ((userChoice == 'scissors') && (compChoice == 'rock')) {
+        alert("Rock beats Scissors! YOU LOSE!")
+    }else if ((userChoice == 'scissors') && (compChoice == 'paper')) {
+        alert("Scissor beat Paper! YOU WIN!") 
+    }
 }
 
 getComputerChoice()
